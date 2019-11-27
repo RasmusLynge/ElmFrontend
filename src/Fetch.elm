9f06f64d054694f18c5a33fd701e1fa8b4c5ac37
RasmusLynge <RasmuslJacobsen@live.com>
2019-11-27 13:01:23 +0100

    first commit from project

---
+++ src/Fetch.elm
@@ -0,0 +1,165 @@
+module Fetch exposing (..)
+
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode exposing (Decoder, field, string)
+
+
+
+-- MAIN
+
+
+main =
+  Browser.element
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    }
+
+
+
+-- MODEL
+
+
+type Model
+  = Failure
+  | Loading
+  | Success String
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  (Loading, getRandomCatGif)
+
+
+
+-- UPDATE
+
+
+type Msg
+  = MorePlease
+  | GotGif (Result Http.Error String)
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    MorePlease ->
+      (Loading, getRandomCatGif)
+
+    GotGif result ->
+      case result of
+        Ok url ->
+          (Success url, Cmd.none)
+
+        Err _ ->
+          (Failure, Cmd.none)
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ h2 [] [ text "Learning to fetch" ]
+    , inputForm model
+    , viewGif model
+    ]
+
+inputForm : Model -> Html Msg
+inputForm model = 
+    div []
+        [ text "Seach for a member here "
+        , br [] []
+        , input [type_ "text", placeholder "search with id" ] []
+        , br [] []
+        , button [ onClick MorePlease ] [ text "Fetch users!" ]
+        , br [] []
+        , br [] []
+        , br [] []
+        , table [] 
+            [
+            tr []
+                [
+                    th[] [text "id "]
+                    ,th[] [text "name "]
+                    , th[] [text "score "]
+                ]
+            ,tr [] 
+                [
+                    td[] [text "12"]
+                    ,td[] [text "Rasmus"]
+                    ,td[] [text "+9000"]
+                ]
+            ]
+        ]
+
+viewGif : Model -> Html Msg
+viewGif model =
+  case model of
+    Failure ->
+      div []
+        [ text "I could not load a random cat for some reason. "
+        , button [ onClick MorePlease ] [ text "Try Cats Again!" ]
+        ]
+
+    Loading ->
+      text "Loading..."
+
+    Success url ->
+      Debug.log(url)
+      div []
+        [ 
+        br [] []
+        , br [] []
+        , br [] []
+        , br [] []
+        , br [] []
+        , br [] []
+        , br [] []
+        , br [] []
+        , br [] []
+        , br [] []
+        , br [] []
+        , br [] []
+        , br [] []
+        , br [] []
+        , br [] []
+        , br [] []
+        , br [] []
+        , br [] []
+        , button [ onClick MorePlease, style "display" "block" ] [ text "More Cats Please!" ]
+        , img [ src url ] []
+        ]
+
+
+
+-- HTTP
+
+
+getRandomCatGif : Cmd Msg
+getRandomCatGif =
+  Http.get
+    { url = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cat"
+    , expect = Http.expectJson GotGif gifDecoder
+    }
+
+
+gifDecoder : Decoder String
+gifDecoder =
+  field "data" (field "image_url" string)
